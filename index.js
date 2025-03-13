709630bb38e5d1f09548b0810a760661def21aa5
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-13 00:43:12 -0700

    Use DB query to get the list of items on homepage

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "Welcome@123",
+  port: 5432,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,11 +22,18 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
-  });
+app.get("/", async (req, res) => {
+  try{
+    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+  
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch(err){
+    console.log(err);
+  }
 });
 
 app.post("/add", (req, res) => {
