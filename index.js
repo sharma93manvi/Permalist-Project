09052e261e597c0c22f9f4aace1ef8d390174c44
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-13 15:31:51 -0700

    Fix errors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
-const db = pg.Client({
+const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "permalist",
@@ -40,7 +40,7 @@ app.post("/add", async (req, res) => {
   const item = req.body.newItem;
   // items.push({ title: item });
   try{
-    await db.query("INSERT INTO items VALUES ($1)", [item]);
+    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
     res.redirect("/");
   }catch(err){
     console.log(err);
@@ -59,7 +59,7 @@ app.post("/edit", async (req, res) => {
 });
 
 app.post("/delete", async(req, res) => {
-  const id = req.body.deleteItemID;
+  const id = req.body.deleteItemId;
   try{
     await db.query("DELETE FROM items WHERE id =$1", [id]);
     res.redirect("/");
