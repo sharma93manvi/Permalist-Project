22c59f203e9036237b6a8d9d0b180ac0e1ea953e
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-13 15:25:17 -0700

    Update items as part of 'edit'

--- index.js
+++ index.js
@@ -47,7 +47,16 @@ app.post("/add", async (req, res) => {
   } 
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  const item = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+  try{
+    await db.query("UPDATE items SET title =($1) WHERE id = $2", [item, id]);
+    res.redirect("/");
+  }catch(err) {
+    console.log(err);
+  }
+});
 
 app.post("/delete", (req, res) => {});
 
